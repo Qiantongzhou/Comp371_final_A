618570d3689ec640a04878db69d6771ab95ecab0
peng2611878 <pengwuyang2years@gmail.com>
2022-08-05 05:18:42 -0400

    update

---
+++ out/build/x64-debug/_deps/thirdparty-glm-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/371/Comp371_final_A/out/build/x64-debug/_deps/thirdparty-glm-src
+# Build directory: C:/371/Comp371_final_A/out/build/x64-debug/_deps/thirdparty-glm-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("glm")
---
+++ out/build/x64-debug/_deps/thirdparty-glm-build/glm/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/371/Comp371_final_A/out/build/x64-debug/_deps/thirdparty-glm-src/glm
+# Build directory: C:/371/Comp371_final_A/out/build/x64-debug/_deps/thirdparty-glm-build/glm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
